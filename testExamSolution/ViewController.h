0703655409f0237c534f3328d34668697c62594f
ViktorsMacbook <viktorsmacbook@ViktorsMacbooks-MacBook-Pro.local>
2017-12-20 17:55:45 +0300

    Initial Commit

---
+++ testExamSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testExamSolution
+//
+//  Created by ViktorsMacbook on 20.12.17.
+//  Copyright © 2017 ViktorsMacbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testExamSolution/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testExamSolution
+//
+//  Created by ViktorsMacbook on 20.12.17.
+//  Copyright © 2017 ViktorsMacbook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
